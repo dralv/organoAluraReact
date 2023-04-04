7eee05923b3366297c494898fdc7b433fc12ceb5
Alvaro <dralvphd2@gmail.com>
2023-04-04 19:40:45 -0300

    Adicionando banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Banner from './Componentes/Banner/Banner'
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <header className="App-header">
+      <h1>Teste</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para recarregar.
         </p>
         <a
           className="App-link"
@@ -19,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
