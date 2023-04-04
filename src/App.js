79fab27efa851bf5569e18f751f4883c381a2694
Alvaro <dralvphd2@gmail.com>
2023-04-04 19:52:26 -0300

    Removendo arquivos desncessários

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
+
 import Banner from './Componentes/Banner/Banner'
 
 function App() {
   return (
-    <div className="App">
-      <Banner/>
-      <header className="App-header">
-      <h1>Teste</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Banner />
     </div>
   )
 }
