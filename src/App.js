c3f57061b24dd07d21a87706b82241778d2551c5
Alvaro <dralvphd2@gmail.com>
2023-04-06 21:39:50 -0300

    Adicionando campo de texto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
-import Banner from './Componentes/Banner/Banner'
+import Banner from './Componentes/Banner'
+import CampoTexto from './Componentes/CampoTexto'
 
 function App() {
   return (
     <div >
       <Banner />
+      <CampoTexto label="Nome" placeholder="Digite o nome"/>
+      <CampoTexto label="Cargo "placeholder="Digite o cargo"/>
+      <CampoTexto label="Image" placeholder="Insira uma imagem"/>
     </div>
   )
 }
 
-export default App
+export default App;
