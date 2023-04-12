7b6ae65d28ad65f9bcc9470b09f84b23a3e5f261
Alvaro <dralvphd2@gmail.com>
2023-04-12 07:32:00 -0300

    Criando dropdown list

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,7 +1,18 @@
 import CampoTexto from '../CampoTexto'
 import './Formulario.css'
+import ListaSuspensa from '../ListaSuspensa'
 
 const Formulario = () => {
+  const times = [
+    'Programação',
+    'Front End',
+    'Data Science',
+    'Devops',
+    'Ux Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +20,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite o nome" />
         <CampoTexto label="Cargo " placeholder="Digite o cargo" />
         <CampoTexto label="Image" placeholder="Insira uma imagem" />
+        <ListaSuspensa label="Time" itens={times} />
       </form>
     </section>
   )
