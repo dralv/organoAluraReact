6bdbc6e25caa9b3401081d308cc6237bba07eabc
Alvaro <dralvphd2@gmail.com>
2023-04-13 18:13:03 -0300

    Alterando estados

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import CampoTexto from '../CampoTexto'
 import './Formulario.css'
 import ListaSuspensa from '../ListaSuspensa'
 import Botao from '../Botao'
+import { useState } from 'react'
 
 const Formulario = () => {
   const times = [
@@ -14,18 +15,46 @@ const Formulario = () => {
     'Inovação e Gestão'
   ]
 
+  const [nome,setNome]= useState('')
+  const [cargo,setCargo]= useState('')
+  const [imagem,setImagem]= useState('')
+  const [time,setTime] = useState('')
+
+
   const aoSalvar = evento => {
     evento.preventDefault()
-    console.log('From foi submetido')
+    console.log('From foi submetido',nome,cargo,imagem,time)
   }
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome" />
-        <CampoTexto obrigatorio={true} label="Cargo " placeholder="Digite o cargo" />
-        <CampoTexto  label="Imagem" placeholder="Insira uma imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <CampoTexto 
+          obrigatorio={true}
+          label="Nome" 
+          placeholder="Digite o nome" 
+          valor={nome}
+          aoAlterado={valor=>setNome(valor)}/>
+
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo "
+          placeholder="Digite o cargo" 
+          valor={cargo}
+          aoAlterado={valor=>setCargo(valor)}/>
+
+        <CampoTexto  
+          label="Imagem" 
+          placeholder="Insira uma imagem" 
+          valor={imagem}
+          aoAlterado={valor=>setImagem(valor)}/>
+
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={times} 
+          valor={time}
+          aoAlterado={valor=>setTime(valor)}/>
         <Botao>Criar card</Botao>
       </form>
     </section>
