6aca471f2d3ab9beca42f971d983b138df083d0e
Alvaro <dralvphd2@gmail.com>
2023-04-18 21:01:30 -0300

    Iniciando componente de times

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-
-import { useState } from 'react';
+import { useState } from 'react'
 import Banner from './Componentes/Banner'
-import Formulario from './Componentes/Formulario';
+import Formulario from './Componentes/Formulario'
+import Time from './Componentes/Time'
 
 function App() {
-  const [colaboradores,setColaboradores]=useState([]);
+  const [colaboradores, setColaboradores] = useState([])
 
-  const aoNovoColaboradorAdicionado = (colaborador)=>{
-    console.log(colaborador);
-    setColaboradores([...colaboradores,colaborador]);
+  const aoNovoColaboradorAdicionado = colaborador => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
   }
-  
+
   return (
-    <div >
+    <div>
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador=>aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario
+        aoColaboradorCadastrado={colaborador =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
+      <Time nome="Programação"></Time>
+      <Time nome="Front-End"></Time>
+      <Time nome="Ux Design"></Time>
     </div>
   )
 }
 
-export default App;
+export default App
