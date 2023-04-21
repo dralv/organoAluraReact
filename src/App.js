de6e1b0d5f45be83aa55474ed166660baf58335a
Alvaro <dralvphd2@gmail.com>
2023-04-21 10:03:34 -0300

    Aprimorando componentes de time

--- src/App.js
+++ src/App.js
@@ -4,6 +4,44 @@ import Formulario from './Componentes/Formulario'
 import Time from './Componentes/Time'
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'Ux Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBAD5',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = colaborador => {
@@ -19,9 +57,10 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
-      <Time nome="Programação"></Time>
-      <Time nome="Front-End"></Time>
-      <Time nome="Ux Design"></Time>
+
+      {times.map(time => (
+        <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}></Time>
+      ))}
     </div>
   )
 }
